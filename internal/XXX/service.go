4ded8d103fda4408ddb690aeadcef5cbd521f2ca
WinBeyond <wangchengchao115@gmail.com>
2022-05-09 10:38:16 +0000

    init project

---
+++ internal/XXX/service.go
@@ -0,0 +1,25 @@
+// Package XXX provides XXX logic implement
+package XXX
+
+import (
+	"fmt"
+)
+
+// XXX
+type XXX struct{
+}
+
+// NewService
+func NewService() *XXX {
+	service := &XXX {
+		// init custom fileds
+	}
+	return service
+}
+
+func (s *XXX) Helloworld() {
+	fmt.Printf("Hello World!\n")
+	// implement business logic here ...
+	// ...
+	return
+}
